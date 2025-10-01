a23ea10f172f7225d2b38a2e42d361a430e06066
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2025-10-01 22:33:32 +0200

    change anther tests

--- DomainUnitTest/Class1.cs
+++ DomainUnitTest/Class1.cs
@@ -4,16 +4,9 @@ using NUnit.Framework;
 
 namespace DomainUnitTest
 {
-    
-
-    [TestFixture]
-    public class ZmianaGrupyStartowejTests
-    {
-        [Test]
-        public void ZmienGrupe_PomyslnaZmiana_ZawodnikPrzeniesiony()
-        {
-            // Arrange
-            var zawodnicy = new List<Zawodnik>
+    public static class PlayersLists
+    { 
+        public static List<Zawodnik> players_15 { get; private set; } = new List<Zawodnik>
             {
                 new Zawodnik("Marcin", "Juranek", 1),
                 new Zawodnik("Anna", "Kowalska", 1),
@@ -31,13 +24,7 @@ namespace DomainUnitTest
                 new Zawodnik("Joanna", "Krawczyk", 1),
                 new Zawodnik("Andrzej", "Piotrowski", 1)
             };
-            var grupaWyjsciowa = new GrupaStartowa(1, 1, "Grupa 1 Dystans 100 KM", 15, false);
-            foreach (var zawodnik in zawodnicy)
-            {
-                grupaWyjsciowa.DodajZawodnika(zawodnik);
-            }
-
-            var zawodnicy2 = new List<Zawodnik>()
+        public static List<Zawodnik> players_14 { get; private set; } = new List<Zawodnik>()
             {
                 new Zawodnik("Robert", "Grabowski", 1),
                 new Zawodnik("Barbara", "Pawlak", 1),
@@ -54,19 +41,37 @@ namespace DomainUnitTest
                 new Zawodnik("Sebastian", "Lis", 1),
                 new Zawodnik("Natalia", "Kalinowska", 1)
             };
+
+    }
+
+    [TestFixture]
+    public class ZmianaGrupyStartowejTests
+    {
+        [Test]
+        public void ChangeGroup_Player_Transferd()
+        {
+            // Arrange
+            var players = PlayersLists.players_15;
+            var startingGroup = new GrupaStartowa(1, 1, "Grupa 1 Dystans 100 KM", 15, false);
+            foreach (var zawodnik in players)
+            {
+                startingGroup.DodajZawodnika(zawodnik);
+            }
+
+            var zawodnicy2 = PlayersLists.players_14;
             var grupaDocelowa = new GrupaStartowa(2, 1, "Grupa 2 Dystans 100 KM", 15, false);
             foreach (var zawodnik in zawodnicy2)
             {
                 grupaDocelowa.DodajZawodnika(zawodnik);
             }
 
-            var zawodnikZmiana = zawodnicy.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
+            var playerChange = players.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
 
             var changeDate = DateTime.Now.AddDays(1);
             var zmiana = new ZmianaGrupyStartowej(
                 changeDate,
-                zawodnikZmiana,
-                grupaWyjsciowa,
+                playerChange,
+                startingGroup,
                 grupaDocelowa
             );
 
@@ -74,75 +79,145 @@ namespace DomainUnitTest
             zmiana.ZmienGrupe();
 
             // Assert
-            grupaWyjsciowa.Zawodnicy.Should().NotContain(zawodnikZmiana);
-            grupaWyjsciowa.IloscMiejscDostepnych.Should().Be(1);
-            grupaDocelowa.Zawodnicy.Should().Contain(zawodnikZmiana);
+            startingGroup.Zawodnicy.Should().NotContain(playerChange);
+            startingGroup.IloscMiejscDostepnych.Should().Be(1);
+            grupaDocelowa.Zawodnicy.Should().Contain(playerChange);
             grupaDocelowa.IloscMiejscDostepnych.Should().Be(0);
  
         }
 
-        //[Test]
-        //public void ZmienGrupe_TerminPrzekroczony_RzucaWyjatek()
-        //{
-        //    // Arrange
-        //    var zawodnik = new Zawodnik { CzyDokonalZmianyGrupy = false };
-        //    var grupaWyjsciowa = new GrupaStartowa { IdDystans = 1 };
-        //    var grupaDocelowa = new GrupaStartowa { IdDystans = 1 };
+        [Test]
+        public void ChangeGroup_Deadline_Exceeded_Throws_An_Exception()
+        {
+            var players = PlayersLists.players_15;
+            var startingGroup = new GrupaStartowa(1, 1, "Grupa 1 Dystans 100 KM", 15, false);
+            foreach (var zawodnik in players)
+            {
+                startingGroup.DodajZawodnika(zawodnik);
+            }
 
-        //    var zmiana = new ZmianaGrupyStartowej(
-        //        DateTime.Now.AddDays(-1), // termin w przeszłości
-        //        zawodnik,
-        //        grupaWyjsciowa,
-        //        grupaDocelowa
-        //    );
+            var zawodnicy2 = PlayersLists.players_14;
+            var grupaDocelowa = new GrupaStartowa(2, 1, "Grupa 2 Dystans 100 KM", 15, false);
+            foreach (var zawodnik in zawodnicy2)
+            {
+                grupaDocelowa.DodajZawodnika(zawodnik);
+            }
 
-        //    // Act & Assert
-        //    var action = () => zmiana.ZmienGrupe();
-        //    action.Should().Throw<DomainException>()
-        //        .WithMessage("Termin zmiany grupy przekroczony");
-        //}
+            var playerChange = players.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
 
-        //[Test]
-        //public void ZmienGrupe_RoznyDystans_RzucaWyjatek()
-        //{
-        //    // Arrange
-        //    var zawodnik = new Zawodnik { CzyDokonalZmianyGrupy = false };
-        //    var grupaWyjsciowa = new GrupaStartowa { IdDystans = 1 };
-        //    var grupaDocelowa = new GrupaStartowa { IdDystans = 2 }; // inny dystans
+            var changeDate = DateTime.Now.AddDays(-1);
+            var zmiana = new ZmianaGrupyStartowej(
+                changeDate,
+                playerChange,
+                startingGroup,
+                grupaDocelowa
+            );
 
-        //    var zmiana = new ZmianaGrupyStartowej(
-        //        DateTime.Now.AddDays(1),
-        //        zawodnik,
-        //        grupaWyjsciowa,
-        //        grupaDocelowa
-        //    );
+            // Act
+            //var action = () => zmiana.ZmienGrupe();
+            //action.Should().Throw<DomainException>()
+            //    .WithMessage("Termin zmiany grupy przekroczony");
+            
+            try
+            {
+                zmiana.ZmienGrupe();
+            }
+            catch (DomainException ex) 
+            {
+                var errorMessage = ex.Message;
+                ex.Message.Should().Be("Termin zmiany grupy przekroczony");
+            }
 
-        //    // Act & Assert
-        //    var action = () => zmiana.ZmienGrupe();
-        //    action.Should().Throw<DomainException>()
-        //        .WithMessage("Grupy mają różne dystanse");
-        //}
 
-        //[Test]
-        //public void ZmienGrupe_ZawodnikJuzDokonalZmiany_RzucaWyjatek()
-        //{
-        //    // Arrange
-        //    var zawodnik = new Zawodnik { CzyDokonalZmianyGrupy = true };
-        //    var grupaWyjsciowa = new GrupaStartowa { IdDystans = 1 };
-        //    var grupaDocelowa = new GrupaStartowa { IdDystans = 1 };
+        }
 
-        //    var zmiana = new ZmianaGrupyStartowej(
-        //        DateTime.Now.AddDays(1),
-        //        zawodnik,
-        //        grupaWyjsciowa,
-        //        grupaDocelowa
-        //    );
+        [Test]
+        public void ZmienGrupe_RoznyDystans_RzucaWyjatek()
+        {
+            var players = PlayersLists.players_15;
+            var startingGroup = new GrupaStartowa(1, 1, "Grupa 1 Dystans 100 KM", 15, false);
+            foreach (var zawodnik in players)
+            {
+                startingGroup.DodajZawodnika(zawodnik);
+            }
 
-        //    // Act & Assert
-        //    var action = () => zmiana.ZmienGrupe();
-        //    action.Should().Throw<DomainException>()
-        //        .WithMessage("Zawodnik dokonał już zmiany grupy");
-        //}
+            var zawodnicy2 = PlayersLists.players_14;
+            var grupaDocelowa = new GrupaStartowa(2, 2, "Grupa 2 Dystans 200 KM", 15, false);
+            foreach (var zawodnik in zawodnicy2)
+            {
+                grupaDocelowa.DodajZawodnika(zawodnik);
+            }
+
+            var playerChange = players.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
+
+            var changeDate = DateTime.Now.AddDays(1);
+            var zmiana = new ZmianaGrupyStartowej(
+                changeDate,
+                playerChange,
+                startingGroup,
+                grupaDocelowa
+            );
+
+            // Act & Assert
+            try
+            {
+                zmiana.ZmienGrupe();
+            }
+            catch (DomainException ex)
+            {
+                var errorMessage = ex.Message;
+                ex.Message.Should().Be("Grupy mają różne dystanse");
+            }
+        }
+
+        [Test]
+        public void ZmienGrupe_ZawodnikJuzDokonalZmiany_RzucaWyjatek()
+        {
+            var players = PlayersLists.players_15;
+            var startingGroup = new GrupaStartowa(1, 1, "Grupa 1 Dystans 100 KM", 15, false);
+            foreach (var zawodnik in players)
+            {
+                startingGroup.DodajZawodnika(zawodnik);
+            }
+
+            var zawodnicy2 = PlayersLists.players_14;
+            var targetGroup = new GrupaStartowa(2, 1, "Grupa 2 Dystans 200 KM", 15, false);
+            foreach (var zawodnik in zawodnicy2)
+            {
+                targetGroup.DodajZawodnika(zawodnik);
+            }
+
+            var playerChange = players.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
+            var changeDate = DateTime.Now.AddDays(1);
+            var zmiana = new ZmianaGrupyStartowej(
+                changeDate,
+                playerChange,
+                startingGroup,
+                targetGroup
+            );
+
+            zmiana.ZmienGrupe();
+
+            var playerChangeAnotherTime = players.FirstOrDefault(x => x.Nazwisko == "Juranek" && x.Imie == "Marcin");
+            var anoterChange = new ZmianaGrupyStartowej(
+                changeDate,
+                playerChange,
+                targetGroup,
+                startingGroup
+            );
+
+            // Act & Assert
+            try
+            {
+                anoterChange.ZmienGrupe();
+            }
+            catch (DomainException ex)
+            {
+                var errorMessage = ex.Message;
+                ex.Message.Should().Be("Zawodnik dokonał już zmiany grupy");
+            }
+
+        }
 
         //[Test]
         //public void ZmienGrupe_BrakMiejsc_RzucaWyjatek()
